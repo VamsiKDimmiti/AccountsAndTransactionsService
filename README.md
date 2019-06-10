e9932d3939cf21128876e3045d1687c722161b24
VamsiKDimmiti <dimmitivamsikrishna@gmail.com>
2019-06-10 21:08:06 +1000

    Commiting a simple Spring boot Application with no functionality in it

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # AccountsAndTransactionsService
 Rest Service to derive all the available accounts and transaction list of an Account
+
+This Restful webservice provide two rest end points
+1. A rest endpoint to view the list of accounts available
+2. A rest endpoint to view the list of transactions for the selected account
---
+++ src/main/java/com/bank/ats/starter/AccountsAndTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.bank.ats.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountsAndTransactionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountsAndTransactionServiceApplication.class, args);
+	}
+
+}
